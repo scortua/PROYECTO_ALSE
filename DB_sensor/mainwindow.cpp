cea5b720593748ecf89ae54b1800ee17810806e8
santiago cortes tovar <scortua@hotmail.com>
2023-12-05 18:29:25 -0500

    ui

--- DB_sensor/mainwindow.cpp
+++ DB_sensor/mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <eigen3/Eigen/Dense>
+#include <QMatrix>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,4 +14,37 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+void MainWindow::on_cmd_Operacion_clicked()
+{
+    //construir matriz de interfaz
+    QMatrix R(3, 6);
+
+    ui->Tmin->setText(QString::number(R(0, 0)));
+    ui->Tprom->setText(QString::number(R(0, 1)));
+    ui->Tmax->setText(QString::number(R(0, 2)));
+
+    ui->Hmin->setText(QString::number(R(1, 0)));
+    ui->Hprom->setText(QString::number(R(1, 1)));
+    ui->Hmax->setText(QString::number(R(1, 2)));
+
+    ui->Vmin->setText(QString::number(R(2, 0)));
+    ui->Vprom->setText(QString::number(R(2, 1)));
+    ui->Vmax->setText(QString::number(R(2, 2)));
+
+    ui->Dmin->setText(QString::number(R(3, 0)));
+    ui->Dprom->setText(QString::number(R(3, 1)));
+    ui->Dmax->setText(QString::number(R(3, 2)));
+
+    ui->Pmin->setText(QString::number(R(4, 0)));
+    ui->Pprom->setText(QString::number(R(4, 1)));
+    ui->Pmax->setText(QString::number(R(4, 2)));
+
+    ui->Lmin->setText(QString::number(R(5, 0)));
+    ui->Lprom->setText(QString::number(R(5, 1)));
+    ui->Lmax->setText(QString::number(R(5, 2)));
+
+}
+
 
---
+++ build-DB_sensor-Desktop-Debug/CMakeFiles/DB_sensor.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DB_sensor_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/DB_sensor_autogen.dir/ParseCache.txt"
+  "DB_sensor_autogen"
+  "CMakeFiles/DB_sensor.dir/DB_sensor_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/DB_sensor.dir/DB_sensor_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/DB_sensor.dir/main.cpp.o"
+  "CMakeFiles/DB_sensor.dir/main.cpp.o.d"
+  "CMakeFiles/DB_sensor.dir/mainwindow.cpp.o"
+  "CMakeFiles/DB_sensor.dir/mainwindow.cpp.o.d"
+  "DB_sensor"
+  "DB_sensor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DB_sensor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
